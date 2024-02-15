dd840b469053fd03a75af55023bf2ee93320a1a0
Subodh-Bny <adhikarisubodh77@gmail.com>
2024-02-15 20:14:20 +0545

    Admin Page Initial Commit

---
+++ admin/index.js
@@ -0,0 +1,31 @@
+//navigate through menu
+let dashboard = document.querySelector(".dashboard");
+let charts = document.querySelector(".charts");
+let manageUsers = document.querySelector(".manage-users");
+let chat = document.querySelector(".chat");
+
+let dashboardLink = document.querySelector("#dashboard");
+let chartsLink = document.querySelector("#charts");
+let manageUsersLink = document.querySelector("#manage-users");
+let chatLink = document.querySelector("#chat");
+
+let active = dashboard;
+function openPage(link, content) {
+  link.addEventListener("click", () => {
+    active.classList.toggle("hide");
+    content.classList.toggle("hide");
+    active = content;
+  });
+}
+openPage(dashboardLink, dashboard);
+openPage(chartsLink, charts);
+openPage(manageUsersLink, manageUsers);
+openPage(chatLink, chat);
+
+//click profile icon
+let profileIcon = document.querySelector(".profile");
+let profileMenu = document.querySelector(".profile-container");
+profileMenu.classList.add("hide");
+profileIcon.addEventListener("click", () => {
+  profileMenu.classList.toggle("hide");
+});
